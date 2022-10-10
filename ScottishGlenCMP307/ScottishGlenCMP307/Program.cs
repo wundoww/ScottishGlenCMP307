294e7ce938d315219020a8f2598bd2d2b9e36d92
Ashley Phillips <2203753@uad.ac.uk>
2022-10-10 18:40:22 +0100

    Added third line of code

---
+++ ScottishGlenCMP307/ScottishGlenCMP307/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScottishGlen_CMP307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Second Line")
+            Console.WriteLine("Third Line")
+        }
+    }
+}
