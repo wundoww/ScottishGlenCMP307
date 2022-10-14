22d0eb3ab9073c886f4a0cfdb79d37c73fe181b3
Ashley Phillips <2203753@uad.ac.uk>
2022-10-14 19:22:03 +0100

    Added book class and 3 properties

---
+++ ScottishGlenCMP307/ScottishGlenCMP307/Book.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ScottishGlen_CMP307
+{
+    class Book
+    {
+        public string Title {get; set;}
+        public List<string> Authors { get; set; }
+        public DateTime PublicationDate { get; set;}
+    }
+}
