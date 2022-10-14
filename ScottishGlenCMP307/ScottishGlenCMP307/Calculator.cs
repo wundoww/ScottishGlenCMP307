24c6a2a4d2e1905c7b27ec97303587a444225889
Ashley Phillips <2203753@uad.ac.uk>
2022-10-14 19:16:27 +0100

    Add calculator class file

---
+++ ScottishGlenCMP307/ScottishGlenCMP307/Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ScottishGlen_CMP307
+{
+    class Calculator
+    {
+        public int Add (int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
