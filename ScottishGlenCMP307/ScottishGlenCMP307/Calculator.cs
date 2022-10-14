5fa5387ea114cbb19eb308a794cd0a00a8c3b639
Ashley Phillips <2203753@uad.ac.uk>
2022-10-14 19:37:13 +0100

    Added subtract method

--- ScottishGlenCMP307/ScottishGlenCMP307/Calculator.cs
+++ ScottishGlenCMP307/ScottishGlenCMP307/Calculator.cs
@@ -8,5 +8,10 @@ namespace ScottishGlen_CMP307
         {
             return left + right;
         }
+
+        public int Subtract (int left, int right)
+        {
+            return left - right;
+        }
     }
 }
